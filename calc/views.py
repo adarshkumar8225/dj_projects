fe3679c78a2bb64679bff5293392a0f9e117f31b
Adarsh Kumar <adarshkumar8225@gmail.com>
2020-05-24 21:10:21 +0530

    static file new website

--- calc/views.py
+++ calc/views.py
@@ -8,7 +8,7 @@ def home(request):
 
 
 def add(request):
-    val1=int(request.POST['num1'])
+    val1=int(request.POST['num1'])#post is used to submit data to the srver
     val2=int(request.POST['num2'])
     res=val1 + val2
 
